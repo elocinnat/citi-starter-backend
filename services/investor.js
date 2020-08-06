40aef2af87eb45f664a97d1bbd24ab9e343d2438
storyinvisible <storyinvisible@gmail.com>
2020-08-06 21:18:25 +0800

    add on export for all the services

--- services/investor.js
+++ services/investor.js
@@ -2,10 +2,16 @@ let MySql = require('sync-mysql');
 
 let dbhandler= require("../utility/dbUtility");
 
+
 let insert = dbhandler.insert;
 let executeQuery= dbhandler.executeQuery
 let update = dbhandler.update
 let select= dbhandler.select
+function twoDigits(d) {
+    if(0 <= d && d < 10) return "0" + d.toString();
+    if(-10 < d && d < 0) return "-0" + (-1*d).toString();
+    return d.toString();
+}
 Date.prototype.toMysqlFormat = function() {
     return this.getUTCFullYear() + "-" + twoDigits(1 + this.getUTCMonth()) + "-" + twoDigits(this.getUTCDate()) + " " + twoDigits(this.getUTCHours()) + ":" + twoDigits(this.getUTCMinutes()) + ":" + twoDigits(this.getUTCSeconds());
 };
@@ -14,16 +20,26 @@ function getInvestorByID(investID){
     return reply[0]
 }
 function investedInStartup(InvestorID, startUpID, amount){
-    insert("investment_log",["startupID","investorID","amount"],[startUpID, InvestorID,amount])
+    let date = new Date();
+    insert("investment_log",["startupID","investorID","amount","date"],[startUpID, InvestorID,amount,date.toMysqlFormat()])
     update_investor_queries="update investor set investedAmt = investedAmt +" +amount.toString()+" where investorID ="+InvestorID.toString()
     executeQuery(update_investor_queries)
     update_startup_queries="update startup set fundsRaised= fundsRaised+" +amount.toString()+" where startupID ="+startUpID.toString()
     executeQuery(update_startup_queries)
-    insert("investment_log",["startupID","investorID","amount"],[startUpID, InvestorID,amount])
 }
 function newInvestor(investorName, amountToInvest){
+    
     insert("investor", ["investorName","sumAmt","investedAmt"],[investorName, amountToInvest,0])
 }
-newInvestor("Zhibobo",10000000)
+function getAllInvestmentByInvestorId(investorID){
+    queries=select("investment_log","*","investorID="+investorID.toString())
+    return queries
+}
+//console.log(getAllInvestmentByInvestorId(160))
+//newInvestor("Zhibobo",10000000)
 //getInvestorByID(160)
-//investedInStartup(160,3,-500)
+//investedInStartup(158,3,-500)
+exports.getAllInvestmentByInvestorId=getAllInvestmentByInvestorId
+exports.getInvestorByID=getInvestorByID
+exports.newInvestor=newInvestor
+exports.investedInStartup=investedInStartup
